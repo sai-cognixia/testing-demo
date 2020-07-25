a277b5b247d93338c5927ff61abb028caf7703bb
sai <sai@devopsguru.in>
2020-07-25 08:54:13 +0530

    my first commit of source code

---
+++ file2.java
@@ -0,0 +1,17 @@
+total 9
+drwxrwxr-x+ 52 root  admin  1664 Jul 24 10:06 Applications
+drwxr-xr-x  68 root  wheel  2176 Jul 17 07:03 Library
+drwxr-xr-x@  8 root  wheel   256 Sep 30  2019 System
+drwxr-xr-x   7 root  admin   224 Sep 30  2019 Users
+drwxr-xr-x   4 root  wheel   128 Jul 24 23:55 Volumes
+drwxr-xr-x@ 38 root  wheel  1216 Jul 17 07:00 bin
+drwxr-xr-x   2 root  wheel    64 Aug 25  2019 cores
+dr-xr-xr-x   3 root  wheel  4438 Jan  1  1970 dev
+lrwxr-xr-x@  1 root  admin    11 Oct 12  2019 etc -> private/etc
+lrwxr-xr-x   1 root  wheel    25 Jul 21 00:41 home -> /System/Volumes/Data/home
+drwxr-xr-x   4 root  wheel   128 Jul 18 19:08 opt
+drwxr-xr-x   6 root  wheel   192 Jul 17 07:01 private
+drwxr-xr-x@ 63 root  wheel  2016 Jul 17 07:00 sbin
+lrwxr-xr-x@  1 root  admin    11 Oct 12  2019 tmp -> private/tmp
+drwxr-xr-x@ 11 root  wheel   352 Oct 12  2019 usr
+lrwxr-xr-x@  1 root  admin    11 Oct 12  2019 var -> private/var
